53253890aa68fd8ddc36bd18d979b84e74057bca
zxlim92 <55634410+zxlim92@users.noreply.github.com>
2020-10-31 12:52:19 -0600

    search functionalities works but not the number of matches

--- main_menu.py
+++ main_menu.py
@@ -1,8 +1,11 @@
 import sqlite3
 import sys
 from PostQuestions import Post
+from SearchPost import SearchMain
 def menu(uid,c):
     option = input("Select an option 1. Post a question 2.Search for post")
     if option =="1" :
         Post(uid,c)
+    elif option =="2":
+        SearchMain(c)
     
