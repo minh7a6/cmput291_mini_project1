f2f08da2b1d89da856e464e45980ca1abe0557e5
Le Minh Dang <mldang@cs.ualberta.ca>
2020-10-31 10:10:36 -0600

    tested vote

--- vote.py
+++ vote.py
@@ -4,18 +4,25 @@ from datetime import date
 
 def giveVote(uid, c):
     pid_tar = input("Put the post ID that you want to vote: ")
-    c.execute("SELECT * FROM posts p WHERE p.pid = ? ", pid_tar)
+    c.execute('''SELECT * FROM posts p WHERE p.pid = (:pid);''', {"pid": pid_tar})
     row = c.fetchone()
-    if(row == []):
+    if row == []:
         print("the post id does not exist, going back to menu...")
         return
-    c.execute("SELECT * FROM votes v WHERE v.pid = ? AND v.uid = ?", pid_tar, uid)
+    c.execute('''SELECT * FROM votes v WHERE v.pid = :pid AND v.uid = :uid;''', {"pid": pid_tar, "uid":uid})
     row = c.fetchone()
-    if(row != []):
+    if row is not None:
         print("you already voted for this posts, going back to menu...")
         return
-    c.execute("SELECT v.vno FROM votes v ORDER BY vno DESC LIMIT 1;")
-    vnoOld = c.fetchall()[0][0]
-    vnoNew = "v"+str(int(vnoOld[1:])+1)
+    c.execute('''SELECT v.vno FROM votes v ORDER BY vno DESC LIMIT 1;''')
+    row = c.fetchone()
+    vnoOld = row[0]
+    vnoNew = vnoOld + 1
+    print(vnoNew)
     c.execute("INSERT INTO votes VALUES(:pid ,:vno , :vdate , :uid)",
                 {"pid":pid_tar, "vno":vnoNew,"vdate":date.today(), "uid":uid})
+
+def func_test():
+    conn = sqlite3.connect('./assignment.db')	
+    c = conn.cursor()
+    giveVote("mldang", c)
